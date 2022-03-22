7ce3e0a9426e57a3fe581092b84138a3dfc09320
Doni Rubiagatra <doni@zero-one-group.com>
2022-03-22 16:54:56 +0700

    feat: add echo web to api

--- service/api/cmd/serve.go
+++ service/api/cmd/serve.go
@@ -6,9 +6,11 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 
 	"github.com/rubiagatra/backend/config"
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -20,11 +22,11 @@ var serveCmd = &cobra.Command{
 	Long:  "server the server",
 	Run: func(cmd *cobra.Command, args []string) {
 		config.GetConfig()
-
-		log.WithFields(log.Fields{
-			"animal": "walrus",
-		}).Info("A walrus appears")
-		fmt.Println(viper.GetString("port"))
+		e := echo.New()
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+		e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", viper.GetString("port"))))
 	},
 }
 
